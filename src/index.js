32923050a54c99b75125d02c96bba797c1dd1e3b
Dylan Curtis <Dcurtisj@gmail.com>
2022-12-01 13:44:53 -0800

    test

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
 // write your code here
+const fetchData = () =>{
+    fetch("http://localhost:3000/ramens")
+    .then(response => response.json())
+    .then(ramen => ramen.forEach (renderMenu))
+}
+const detail = document.getElementById("ramen-detail")
+fetchData()
+
+// finders
+const menu = document.getElementById("ramen-menu")
+
+// create
+
+
+// append
+
+
+const renderMenu = (menuObj) => {    
+    const menu = document.getElementById("ramen-menu")
+    const MenuImg = document.createElement("img")
+    MenuImg.addEventListener("click", () =>{
+        detail.src = e.target.src
+        detail.name.innerText = e.target.name
+    })
+    menu.appendChild(MenuImg)
+    MenuImg.src = menuObj.image
+    
+}
+
+const updateDetail = () => {    
+    const detImg = document.createElement("img")
+    const detName = document.createElement("h2")
+    const detRes = document.createElement("h3")
+    detail.appendChild(detName,detImg,detRes)
+}
